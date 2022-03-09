65b85d575ba9d530acd7e0a756d1ae9e5824ed6b
Mario Sessa <mariosessa63@gmail.com>
2022-03-09 21:29:30 +0100

    Complete TicTacToeTomek solution

--- TicTacToeTomek/Solution.java
+++ TicTacToeTomek/Solution.java
@@ -1,143 +1,104 @@
-import java.io.OutputStream;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.BufferedWriter;
-import java.util.InputMismatchException;
-import java.io.IOException;
-import java.io.Writer;
-import java.io.OutputStreamWriter;
-import java.io.InputStream;
-import java.lang.Math;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
- * Built using CHelper plug-in
- * Actual solution is at the top
- *
+ * 
  * @author kode
  */
 public class Solution {
-    public static void main(String[] args) {
-        InputStream inputStream = System.in;
-        OutputStream outputStream = System.out;
-        InputReader in = new InputReader(inputStream);
-        OutputWriter out = new OutputWriter(outputStream);
-        ProblemSolver solver = new ProblemSolver();
-        int i = in.nextInt();
-        solver.solve(i, in, out);
-        out.close();
-    }
-
-
-    // Algorithm here
-    static class ProblemSolver {
-        public void solve(int num, InputReader in, OutputWriter out) {
-            
-            out.printf("%d", num);
+    private static final boolean DEBUG = false;
+    private static String solve(int count, int dim,  char[][] matrix) {
+        int result = 0;
+        boolean point = false;
+        boolean t = false;
+        int xt = -1, yt = -1;
+        int[][] val = new int[dim][dim];
+        for(int i = 0; i < dim; i++){
+            for(int j = 0; j < dim; j++){
+                if(matrix[i][j] == '.') {
+                    val[i][j] = 0;
+                    point = true;
+                }
+                if(matrix[i][j] == 'O') val[i][j] = 1;
+                if(matrix[i][j] == 'X') val[i][j] = -1;
+                if(matrix[i][j] == 'T') {
+                    val[i][j] = 0; 
+                    t = true;
+                    xt = i;
+                    yt = j;
+                }
+            }
         }
 
-    }
-
-    static class OutputWriter {
-        private final PrintWriter writer;
-
-        public OutputWriter(OutputStream outputStream) {
-            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
-        }
-
-        public OutputWriter(Writer writer) {
-            this.writer = new PrintWriter(writer);
-        }
-
-        public void printf(String format, Object... objects) {
-            writer.printf(format, objects);
-        }
-
-        public void close() {
-            writer.close();
-        }
-
-    }
-
-    static class InputReader {
-        private InputStream stream;
-        private byte[] buf = new byte[1 << 16];
-        private int curChar;
-        private int numChars;
-
-        public InputReader(InputStream stream) {
-            this.stream = stream;
-        }
-
-        public int read() {
-            if (this.numChars == -1) {
-                throw new UnknownError();
+        // control rows and cols
+        int sumR = 0;
+        int sumC = 0;
+        int sumD = 0;
+        int sumD2 = 0;
+        for(int i = 0; i < dim; i++){
+            sumR = 0;
+            sumC = 0;
+            sumD += val[i][i];
+            sumD2 += val[i][dim - i - 1];
+            for(int j = 0; j < dim; j++){
+                sumR += val[i][j];
+                sumC += val[j][i];
+            }
+            if(sumR == 4 || sumC == 4 || sumD == 4 || sumD2 == 4){
+                result = 2;
+            } else if(sumR == -4 || sumC == -4 || sumD == -4 || sumD2 == -4){
+                result = 1;
             } else {
-                if (this.curChar >= this.numChars) {
-                    this.curChar = 0;
-
-                    try {
-                        this.numChars = this.stream.read(this.buf);
-                    } catch (IOException var2) {
-                        throw new InputMismatchException();
+                if(t){
+                    if((sumR == 3  && xt == i)|| (sumC == 3 && yt == i) || (sumD == 3 && xt == yt) || (sumD2 == 3 && (xt == (dim - yt - 1)))){
+                        result = 2;
+                    } else if((sumR == -3 && xt == i) || (sumC == -3 && yt == i) || (sumD == -3 && xt == yt) || (sumD2 == -3 && (xt == (dim - yt - 1)))){
+                        result = 1;
                     }
-
-                    if (this.numChars <= 0) {
-                        return -1;
-                    }
-                }
-
-                return this.buf[this.curChar++];
-            }
-        }
-
-        public int nextInt() {
-            int c;
-            for (c = this.read(); isSpaceChar(c); c = this.read()) {
-                ;
-            }
-
-            byte sgn = 1;
-            if (c == 45) {
-                sgn = -1;
-                c = this.read();
-            }
-
-            int res = 0;
-
-            while (c >= 48 && c <= 57) {
-                res *= 10;
-                res += c - 48;
-                c = this.read();
-                if (isSpaceChar(c)) {
-                    return res * sgn;
+                    
                 }
             }
-
-            throw new InputMismatchException();
         }
 
-        public String next() {
-            int c;
-            while (isSpaceChar(c = this.read())) {
-                ;
+        String c2;
+        switch(result){
+            case 0: if(point) c2 = "Game has not completed"; else c2 = "Draw"; break;
+            case 1: c2 = "X won"; break;
+            case 2: c2 = "O won"; break;
+            default: c2 = "";
+        }
+
+        return c2;
+
+
+
+
+    }
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        long beginTime = System.nanoTime();
+        InputStream is = DEBUG ? new FileInputStream("") : System.in;
+        try (Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(is)))) {
+            int testCount = scanner.nextInt();
+            for(int i = 0; i < testCount; i++){
+                char[][] matrix = new char[4][4];
+                for(int j = 0; j < 4; j++){
+                    String row = scanner.next();
+                    for(int k = 0; k < 4; k++){
+                        matrix[j][k]= row.charAt(k);
+                    }
+                }
+                String result = solve(i, 4, matrix);
+                System.out.println("Case #" + i + ": " + result);
             }
-
-            StringBuilder result = new StringBuilder();
-            result.appendCodePoint(c);
-
-            while (!isSpaceChar(c = this.read())) {
-                result.appendCodePoint(c);
-            }
-
-            return result.toString();
         }
-
-        public static boolean isSpaceChar(int c) {
-            return c == 32 || c == 10 || c == 13 || c == 9 || c == -1;
-        }
-
+        
+        System.err.println( "Done in " + ((System.nanoTime() - beginTime) / 1e9) + " seconds.");
     }
 }
-
